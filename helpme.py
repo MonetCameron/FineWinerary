b03cad8e70d883ee3b559e5b69bb08484c4edb1f
MonetCameron <160192219+MonetCameron@users.noreply.github.com>
2024-10-21 20:42:02 -0400

    Update helpme.py

--- helpme.py
+++ helpme.py
@@ -10,7 +10,7 @@ def home_page():
     st.header("This app helps you find the perfect wine to pass the time.")
     st.subheader("Use the navigation menu to access the survey and get personalized recommendations.")
 
-    image_path = '/Users/monetcameron/Desktop/1674384291032.jpg'  # Replace with your image path
+    image_path = 'file:///Users/monetcameron/Desktop/1674384291032.jpg'  # Replace with your image path
     image = Image.open(image_path)
     st.image(image, caption='This is a local image.', use_column_width=True)
 
